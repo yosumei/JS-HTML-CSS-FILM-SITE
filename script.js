0ae16e341e9128d5100338cd9ab09fea397a792c
yosumei <nurish60@gmail.com>
2023-10-25 17:27:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    let clickCounter = 0;
+    const imageItem = movieLists[i].querySelectorAll("img").length;
+    arrow.addEventListener("click", function () {
+    console.log(imageItem);
+    clickCounter++;
+    if (imageItem - (4 + clickCounter) >= 0)
+        
+            clickCounter++;
+                    movieLists[i].style.transform = 
+                    `translateX(${
+                    movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`
+                });
+    
+
+});
+
+
+/* dark mode  */
+
+const ball=document.querySelector(".toggle-ball");
+const items= document.querySelectorAll(".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title")
+
+ball.addEventListener("click",function() {
+   items.forEach((item) => item.classList.toggle("active"))
+})
+
+
